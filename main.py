1b6d11ffa8f1ea77b851474e486071c6c2949b53
lakers2 <35631341+lakers2@users.noreply.github.com>
2024-08-27 21:39:39 +0800

    add CORS resource get

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://你的前端域名.com"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
