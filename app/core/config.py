2abd9254f45ef937abaca3c252f8fd3c4951c883
lakers2 <35631341+lakers2@users.noreply.github.com>
2024-08-27 22:11:33 +0800

    change for railway deploy

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,9 @@
 from pydantic_settings import BaseSettings
+import os
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "FastAPI Project"
-    DATABASE_URL: str = "sqlite:///./sql_app.db"
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
 
     class Config:
         env_file = ".env"
